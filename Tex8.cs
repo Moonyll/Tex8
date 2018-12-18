1c26b494c0ebf90d5ee0de4832d3fdf11ce267b9
Moonyll <s.pouwel@laposte.net>
2018-12-18 11:39:55 +0100

    Tex8

---
+++ Tex8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        enum week
+        {
+            lundi,
+            mardi,
+            mercredi,
+            jeudi,
+            vendredi,
+            saemdi,
+            dimanche
+        }
+        static void Main(string[] args)
+        {
+         // foreach (int i in Enum.GetValues(typeof(week)))
+         foreach (string i in Enum.GetNames(typeof(week)))
+         Console.WriteLine(i);
+         Console.ReadKey();
+         Console.WriteLine(Enum.GetName(typeof(week), 4));
+            // Console.WriteLine(Enum.ToObject(typeof(week), 0));
+         Console.WriteLine("1"+ "\n"+ Enum.GetName(typeof(week),1) + "\n" + Enum.GetName(typeof(week), 2) + "\n" + Enum.GetName(typeof(week), 3) + "\n" + Enum.GetName(typeof(week),4) + "\n" + Enum.GetName(typeof(week), 5) + "\n" + Enum.GetName(typeof(week), 6));
+        }
+    }
+}
